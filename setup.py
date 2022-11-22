86dd6567732591e0cae20c62779a8c435e21fb5d
Diego Ferigo <diego.ferigo@iit.it>
2022-11-22 12:06:29 +0100

    Create setup.cfg file and mode data from setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 1/10/21
-# @Author  : Daniel Ordoñez
-# @email   : daniels.ordonez@gmail.com
+import setuptools
 
-from distutils.core import setup
-from setuptools import find_packages
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setup(name='slip-control',
-      version='0.0.1',
-      description='Spring Loaded Inverted Pendulum control and visualization python tools',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      author='Daniel Ordonez',
-      author_email='daniel.ordonez@gmail.com',
-      url='https://github.com/Danfoa/slip_control',
-      packages=find_packages(),
-      classifiers=[
-          "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: MIT License",
-          "Operating System :: OS Independent",
-      ],
-      requires=['numpy', 'scipy', 'matplotlib', 'tqdm', 'cvxpy'],
-      python_requires=">=3.6",
-      )
+setuptools.setup()
